50a2b89f4abec0256f59c9555eea4db983dcdd0c
Ivan <vanya.korobov@yandex.ru>
2025-02-20 22:27:57 +0300

    test-commit

---
+++ src/main/java/org/example/service/interaction.java
@@ -0,0 +1,9 @@
+package org.example.service;
+
+public interface interaction {
+    void addContact();
+    void removeContact();
+    void getAllContacts();
+    void searchContact();
+    void getContactOfGroup();
+}
---
+++ src/main/java/org/example/service/phone.java
@@ -0,0 +1,46 @@
+package org.example.service;
+
+import org.example.model.Contact;
+
+import java.util.*;
+
+public class phone implements interaction {
+
+    Scanner scanner = new Scanner(System.in);
+
+    String phone;
+    String name;
+    String emale;
+    String group;
+    List<Contact> contacts = new ArrayList<Contact>();
+    Map<String, Contact> contactMap = new HashMap<String, Contact>();
+
+
+    public void phone() {
+
+    }
+    @Override
+    public void addContact() {
+
+    }
+
+    @Override
+    public void removeContact() {
+
+    }
+
+    @Override
+    public void getAllContacts() {
+
+    }
+
+    @Override
+    public void searchContact() {
+
+    }
+
+    @Override
+    public void getContactOfGroup() {
+
+    }
+}
